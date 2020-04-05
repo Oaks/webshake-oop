07839aa9d9b44b0fb82bdbc20a5dab461198303b
Sergey Tvarnov <bropstk@gmail.com>
2020-04-05 05:22:53 +0300

    Moved the sensitive data to a file .env

--- src/MyProject/Services/EmailSender.php
+++ src/MyProject/Services/EmailSender.php
@@ -45,13 +45,13 @@ class EmailSender
             $mail->isSMTP();                                            // Send using SMTP
             $mail->Host       = 'smtp.mailtrap.io';                     // Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-            $mail->Username   = 'a6cbb04e6a420f';                       // SMTP username
-            $mail->Password   = '230e793fed53d5';                       // SMTP password
+            $mail->Username   = getenv('MAILER_USER');                       // SMTP username
+            $mail->Password   = getenv('MAILER_PASSWORD');                       // SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
             $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
             //Recipients
-            $mail->setFrom('from@webshake.com', 'Mailer');
+            $mail->setFrom(getenv('MAILER_FROM_EMAIL'), 'Mailer');
             $mail->addAddress($to, 'Joe User');     // Add a recipient Name is optional
 
             // Content
--- src/templates/mail/userActivation.php
+++ src/templates/mail/userActivation.php
@@ -1,3 +1,3 @@
 Добро пожаловать на наш портал!<br>
-Для активации вашего аккаунта нажмите <a href="http://myproject.loc/users/<?=$userId?>/activate/<?=$code?>">сюда</a>.
+Для активации вашего аккаунта нажмите <a href="<?=getenv('FRONTEND_URL')?>/users/<?=$userId?>/activate/<?=$code?>">сюда</a>.
 
