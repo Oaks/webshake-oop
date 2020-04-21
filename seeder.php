156e66ab37cd25cc8173f2f0f0d9a297a96b3bbc
Sergey Tvarnov <bropstk@gmail.com>
2020-04-21 22:33:45 +0300

    Seed users

---
+++ seeder.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use MyProject\Models\Articles\Article;
+use MyProject\Models\Users\User;
+
+$USER = 'MyProject\Models\Users\User';
+
+function bindObj($obj, $class) {
+    return function($property, $value) use ($obj, $class) {
+        Closure::bind(function($v) use ($property) {$this->{$property} = $v;} , $obj, $class)($value);
+    };
+}
+
+// use the factory to create a Faker\Generator instance
+$faker = Faker\Factory::create();
+
+$users = [];
+for ($i = 0; $i < 2; $i++) {
+    $user = new User;
+    $bindUser = bindObj($user, $USER);
+
+    $bindUser( 'id',            null);
+    $bindUser( 'nickname',      lcfirst($faker->firstName));
+    $bindUser( 'email',         $faker->email);
+    $bindUser( 'isConfirmed',   $faker->boolean);
+    $bindUser( 'role',          $faker->boolean ? 'admin' : 'user');
+    $bindUser( 'passwordHash',  '$2y$10$217fthMDr6ChcKIJIABvNOi0G2fDtGD1QMOuPPjboetzY8mKVt7nq');
+    $bindUser( 'auth_token',    'auth_token');
+    $bindUser( 'createdAt',     $faker->dateTime->format('Y-m-d H:i:s'), 'Europe/Kyiv');
+
+    $user->save();
+
+
+    $users[] = $user;
+}
+
+echo count($users) . PHP_EOL;
+var_export($users);
+
+$article = new Article();
