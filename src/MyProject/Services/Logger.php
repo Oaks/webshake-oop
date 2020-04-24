5f0db84e9c579235b8f9a2aeb924228f3adbe672
Sergey Tvarnov <bropstk@gmail.com>
2020-04-24 22:09:34 +0300

    Another logger

--- src/MyProject/Services/Logger.php
+++ src/MyProject/Services/Logger.php
@@ -4,8 +4,9 @@ namespace MyProject\Services;
 
 trait Logger {
     function log($msg) {
-        echo '<pre>';
-        echo date('Y-m-d h:i:s') . ':' . '(' . __CLASS__. ') ' . $msg . '<br/>';
-        echo '</pre>';
+        /* echo '<pre>'; */
+        /* echo date('Y-m-d h:i:s') . ':' . '(' . __CLASS__. ') ' . $msg . '<br/>'; */
+        /* echo '</pre>'; */
+        echo sprintf("<pre>%s:(%s) %s<br/></pre>", date('Y-m-d h:i:s'), __CLASS__, $msg );
     }
 }
