64e24f081e9c13b375c2f79efb0af3236893744e
Sergey Tvarnov <bropstk@gmail.com>
2020-04-30 23:04:45 +0300

    Split starter.html on template & content
        AdminController render splitted starter* in whole page

--- src/MyProject/Controllers/Admin/AdminController.php
+++ src/MyProject/Controllers/Admin/AdminController.php
@@ -9,6 +9,21 @@ class AdminController extends AbstractController
     public function index()
     {
         /* $this->log($this->templates); */
-        echo file_get_contents($this->templates . '/admin/layouts/starter.html');
+        /* echo file_get_contents($this->templates . '/admin/layouts/starter.html'); */
+        echo $this->get_content($this->templates . '/admin/layouts/starter.html', [ 
+            'content' => file_get_contents($this->templates . '/admin/starter-content.html')
+        ]);
+    }
+
+    protected function get_content($file, $data)
+    {
+        $template = file_get_contents($file);
+
+        foreach($data as $key => $value)
+        {
+            $template = str_replace('{{'.$key.'}}', $value, $template);
+        }
+
+        return $template;
     }
 }
